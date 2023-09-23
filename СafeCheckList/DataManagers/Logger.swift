0427733d96cb90afedf2f4d51db95e390352edac
Yury Lebedev <quasaryy@gmail.com>
2023-09-23 18:02:10 +0400

    App is ready

---
+++ СafeCheckList/DataManagers/Logger.swift
@@ -0,0 +1,8 @@
+//
+//  Logger.swift
+//  CafeCheckList
+//
+//  Created by Yury on 23/09/2023.
+//
+
+import Foundation
---
+++ СafeCheckList/DataManagers/StorageManager.swift
@@ -0,0 +1,8 @@
+//
+//  StorageManager.swift
+//  CafeCheckList
+//
+//  Created by Yury on 23/09/2023.
+//
+
+import Foundation
